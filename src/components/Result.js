57e349d4db633dc2cc2230c38a7e3b8a38e610ae
gseung02 <jiseung1102@naver.com>
2025-02-14 16:54:36 +0900

    updatesize

--- src/components/Result.js
+++ src/components/Result.js
@@ -5,7 +5,7 @@ const Result = ({score,onExit}) => {
         축하합니다!
       </h2>
       <h2>총 {score}문제를 맞히셨습니다.</h2>
-      <img src="./image/firecracker.png" alt="축하 이미지"></img>
+      <img src="/image/firecracker.png" alt="축하 이미지"/>
       <button onClick={onExit}>처음으로</button>
     </div>
   );
