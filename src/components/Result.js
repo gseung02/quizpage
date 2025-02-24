12a137e323287fa0960d5f5b07f543e89a2b0868
gseung02 <jiseung1102@naver.com>
2025-02-24 10:16:23 +0900

    updateimg

--- src/components/Result.js
+++ src/components/Result.js
@@ -5,7 +5,7 @@ const Result = ({score,onExit}) => {
         축하합니다!
       </h2>
       <h2>총 {score}문제를 맞히셨습니다.</h2>
-      <img src="/image/firecracker.png" alt="축하 이미지"/>
+      <img src={`${process.env.PUBLIC_URL}/image/firecracker.png`} alt="축하이미지" />
       <button onClick={onExit}>처음으로</button>
     </div>
   );
